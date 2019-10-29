413973c76e77ecdc8060aa942d3844d341907c52
chenjunhua <chenjunhua@qingzu.com.cn>
2019-10-29 19:14:36 +0800

    对接上电影接口~艰辛呀各种坑(图片403,base64不显示...)

--- pages/movie/movie.vue
+++ pages/movie/movie.vue
@@ -2,7 +2,7 @@
 	<view class="movie-main">
 		<view class="movie-body">
 			<view class="movie-body-left">
-				<image :src="movie.img"></image>
+				<image :src="movie.img" mode="aspectFit"></image>
 			</view>
 			<view class="movie-body-right">
 				<view class="movie-title">{{movie.name}}</view>
@@ -16,10 +16,11 @@
 		<view class="movie-score">豆瓣评分: {{movie.score}}</view>
 		<view class="movie-desc">
 			<view class="movie-desc-title">剧情简介</view>
-			<view class="movie-desc-value">{{movie.desc}}</view>
+			<view class="movie-desc-value">{{desc}}</view>
 		</view>
 		<view class="movie-comments">
-			<view class="movie-comments-item" v-for="item in movie.components">
+			<view class="movie-comments-title">评论区</view>
+			<view class="movie-comments-item" v-for="item in comments" :key="item.id">
 				{{item}}
 			</view>
 		</view>
@@ -30,22 +31,48 @@
 	export default {
 		data() {
 			return {
-				movie: ''
+				movie: {
+					id: '',
+					name: '',
+					desc: '',
+					actors: '',
+					img: '',
+					url: '',
+					score: 0,
+					comments: []
+				},
+				desc: '',
+				comments: [],
 			}
 		},
 		onLoad() {
+			//解决图片403
+			const oMeta = document.createElement('meta');
+			oMeta.name = "referrer";
+			oMeta.content = "no-referrer"
+			document.getElementsByTagName('head')[0].appendChild(oMeta);
+			console.log("set meta: " + oMeta);
+
+			const that = this;
 			//获取电影
 			uni.request({
 				url: 'http://h5.springeasy.cn/mp/cloud/getNowPlaying',
 				success(res) {
+					console.log(res);
 					if (res.data.code == 200) {
-						this.movie = res.data.data;
+						that.movie = res.data.data;
+						console.log("this.movie：" + that.movie);
 						//获取描述
 						uni.request({
 							url: 'http://h5.springeasy.cn/mp/cloud/getMovieDesc',
+							data: {
+								movieId: that.movie.id
+							},
 							success(res) {
+								console.log(res);
 								if (res.data.code == 200) {
-									this.movie.desc = res.data.data;
+									that.desc = res.data.data;
+									console.log("this.desc: " + that.desc);
 								} else {
 									uni.showToast({
 										icon: 'none',
@@ -57,9 +84,16 @@
 						//获取评论
 						uni.request({
 							url: 'http://h5.springeasy.cn/mp/cloud/getComments',
+							data: {
+								movieId: that.movie.id,
+								pageNum: 1,
+								pageSize: 20
+							},
 							success(res) {
+								console.log(res);
 								if (res.data.code == 200) {
-									this.movie.comments = res.data.data;
+									that.comments = res.data.data;
+									console.log("this.comments: " + that.comments);
 								} else {
 									uni.showToast({
 										icon: 'none',
@@ -76,10 +110,25 @@
 					}
 				}
 			});
+		},
+		onPullDownRefresh() {
+			console.log('refresh');
+			setTimeout(function() {
+				uni.stopPullDownRefresh();
+			}, 1000);
 		}
 	}
 </script>
 
 <style>
-
+	.movie-body {
+		position: relative;
+		margin-bottom: 10px;
+	}
+	.movie-body-left {
+		display: inline-block;
+	}
+	.movie-body-right{
+		display: inline-block;
+	}
 </style>
